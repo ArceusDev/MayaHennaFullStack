8bff51e0c6770f287cdc2784b691f91541754757
Hamza Afzal <95589034+ArceusDev@users.noreply.github.com>
2024-02-17 23:25:51 +0500

    Auth created

---
+++ api/Data/MS_DbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace api.Data
+{
+    public class MS_DbContext : IdentityDbContext
+    {
+        public MS_DbContext(DbContextOptions<MS_DbContext> options) : base(options) 
+        { 
+
+        }
+    }
+}
